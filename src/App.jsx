64f72047fdfef9e3e27af27a4f5fab19aee76e65
Syed GM Ibrahim <syedgmibrahim@Syeds-MacBook-Air.local>
2025-05-17 15:11:42 +0530

    State and localStorage added (Sai)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import EmployeeDashboard from "./pages/EmployeeDashboard";
-import Tasks from "./pages/Tasks";
+import React, {useContext, useEffect, useState} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import AdminDashboard from './pages/AdminDashboard';
+import EmployeeDashboard from './pages/EmployeeDashboard';
+import Tasks from './pages/Tasks';
+import {AuthContext} from './context/AuthProvider';
 
 function App() {
+  const [user, setUser] = useState (null);
+  const [loggedInUserData, setLoggedInUserData] = useState (null);
+  const [userData, SetUserData] = useContext (AuthContext);
+
+  useEffect (() => {
+    const loggedInUser = localStorage.getItem ('loggedInUser');
+    if (loggedInUser) {
+      const userData = JSON.parse (loggedInUser);
+      setUser (userData.role);
+      setLoggedInUserData (userData.data);
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
